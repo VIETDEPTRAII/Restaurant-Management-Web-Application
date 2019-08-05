f1eafb07bd149dfea049a68d7dceee76b256c16b
DoMinhTam <dominhtam@gmail.com>
2019-08-05 20:07:54 +0700

    add project

---
+++ WebService/QuanLyNhaHang/src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,176 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.LoaiNguyenLieuService;
+import com.example.demo.Service.NguyenLieuService;
+import com.example.demo.model.LoaiNguyenLieu;
+import com.example.demo.model.NguyenLieu;
+
+import ch.qos.logback.classic.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class WebController {
+	@Autowired
+	LoaiNguyenLieuService repositoryLoaiNguyenLieu;
+	@Autowired
+	NguyenLieuService repositoryNguyenLieu;
+	
+	/////////////////////////////// LOAI NGUYEN LIEU///////////////
+	@RequestMapping(path = "/get_list_loainguyenlieu", produces = MediaType.APPLICATION_JSON_VALUE)
+	public java.util.List<LoaiNguyenLieu> getAllLoaiMonAn() {
+		// This returns a JSON or XML with the users
+		return repositoryLoaiNguyenLieu.findAll();
+	}
+
+	@RequestMapping(value = "/GetAllLoaiNguyenLieu", method = RequestMethod.GET)
+	public ResponseEntity<List<LoaiNguyenLieu>> listAllLoaiNguyenLieu() {
+		    List<LoaiNguyenLieu> listLoaiNguyenLieu = repositoryLoaiNguyenLieu.findAll();
+		if (listLoaiNguyenLieu.isEmpty()) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		// return ResponseEntity<List<Contact>>(listContact, HttpStatus.OK);
+		return new ResponseEntity<List<LoaiNguyenLieu>>(listLoaiNguyenLieu, HttpStatus.OK);
+	}
+
+	// lay 1 loai nguyen lieu
+	@RequestMapping(value = "/LoaiNguyenLieu/{id}", method = RequestMethod.GET)
+
+	public LoaiNguyenLieu findLoaiNguyenLieuByID(@PathVariable("id") long id) {
+		   LoaiNguyenLieu loainguyenlieu = repositoryLoaiNguyenLieu.getOne(id);
+		    if (loainguyenlieu == null) {
+			      ResponseEntity.notFound().build();
+		    }
+		return loainguyenlieu;
+	}
+	
+	//update loai nguyen lieu
+	@RequestMapping(value = "/UpdateLoaiNguyenLieu/", 
+			method = RequestMethod.POST)
+	public ResponseEntity<LoaiNguyenLieu> updateLoaiNguyenLieu(@Valid @RequestBody LoaiNguyenLieu loainguyenlieuForm) {
+		LoaiNguyenLieu lnl = repositoryLoaiNguyenLieu.getOne(loainguyenlieuForm.getId());
+	    if(lnl == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    lnl.setLOAINGUYENLIEU_NAME(loainguyenlieuForm.getLOAINGUYENLIEU_NAME());
+	    lnl.setLOAINGUYENLIEU_UNIT(loainguyenlieuForm.getLOAINGUYENLIEU_UNIT());
+
+	    LoaiNguyenLieu updatedLoaiNguyenLieu = repositoryLoaiNguyenLieu.save(lnl);//update trong database
+	    return ResponseEntity.ok(updatedLoaiNguyenLieu);
+	}
+  //xóa loai nguyen lieu
+	@RequestMapping(value = "/DeleteLoaiNguyenLieu", method = RequestMethod.POST)
+	public int deleteLoaiNguyenLieu(@Valid @RequestBody LoaiNguyenLieu loainguyenlieu) {
+		//@PathVariable(value=""): lay bien tu url
+		//@RequestBody: lay object duoc gui trong body
+		LoaiNguyenLieu lnl = repositoryLoaiNguyenLieu.getOne(loainguyenlieu.getId());
+	    if(lnl == null) {
+	        return 0;
+	    }
+	    repositoryLoaiNguyenLieu.delete(lnl);//delete trong database
+	    return 1;
+	}
+	//Them loai nguyen lieu
+	@RequestMapping(
+			value = "/InsertLoaiNguyenLieu/", 
+			method = RequestMethod.POST
+			
+			)
+	@ResponseBody
+	public LoaiNguyenLieu insertLoaiNguyenLieu(@Valid @RequestBody LoaiNguyenLieu loainguyenlieuForm) {
+		//@Valid: kiem tra xem co ton tai object trong body
+		LoaiNguyenLieu lnl=repositoryLoaiNguyenLieu.save(loainguyenlieuForm);
+		return lnl;
+	}
+	
+	
+/////////////////////////////// NGUYEN LIEU/ //////////////
+	
+	@RequestMapping(path = "/GetAllNguyenLieu", produces = MediaType.APPLICATION_JSON_VALUE)
+	public java.util.List<NguyenLieu> getAllNguyenLieu() {
+		// This returns a JSON or XML with the users
+		return repositoryNguyenLieu.findAll();
+	}
+
+	
+
+	// lay 1  nguyen lieu
+	@RequestMapping(value = "/NguyenLieu/{id}", method = RequestMethod.GET)
+
+	public NguyenLieu findNguyenLieuByID(@PathVariable("id") long id) {
+		   NguyenLieu nguyenlieu = repositoryNguyenLieu.getOne(id);
+		    if (nguyenlieu == null) {
+			      ResponseEntity.notFound().build();
+		    }
+		return nguyenlieu;
+	}
+	
+	//update  nguyen lieu
+	@RequestMapping(value = "/UpdateNguyenLieu", 
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<NguyenLieu> updateNguyenLieu(@Valid NguyenLieu nguyenlieuForm) {
+		NguyenLieu nl = repositoryNguyenLieu.getOne(nguyenlieuForm.getNGUYENLIEU_ID());
+	    if(nl == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    
+	    nl.setNGUYENLIEU_NAME(nguyenlieuForm.getNGUYENLIEU_NAME());
+	    nl.setNGUYENLIEU_PRICE(nguyenlieuForm.getNGUYENLIEU_PRICE());
+        nl.setNGUYENLIEU_DATE(nguyenlieuForm.getNGUYENLIEU_DATE());
+       
+	    NguyenLieu updatedNguyenLieu = repositoryNguyenLieu.save(nl);//update trong database
+	    return ResponseEntity.ok(updatedNguyenLieu);
+	}
+  //xóa nguyen lieu
+	//cach 1
+	@RequestMapping(value = "/NguyenLieu/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<NguyenLieu> deleteNguyenLieu1(@PathVariable(value = "id") Long id) {
+		NguyenLieu nl = repositoryNguyenLieu.getOne(id);
+	    if(nl == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    repositoryNguyenLieu.delete(nl);//delete trong database
+	    return ResponseEntity.ok().build();
+	}
+	
+	//cach 2
+	@RequestMapping(value = "/DeleteNguyenLieu", method = RequestMethod.POST)
+	public int deleteNguyenLieu(@Valid @RequestBody  NguyenLieu nguyeLieu) {
+		NguyenLieu nl = repositoryNguyenLieu.getOne(nguyeLieu.getNGUYENLIEU_ID());
+	    if(nl == null) {
+	        return 0;
+	    }
+	    repositoryNguyenLieu.delete(nl);//delete trong database
+	    return 1;
+	}
+	//Them nguyen lieu
+	@RequestMapping(
+			value = "/InsertNguyenLieu", 
+			method = RequestMethod.POST,
+			produces = { MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+			)
+	@ResponseBody
+	public NguyenLieu insertNguyenLieu( NguyenLieu nguyenlieuForm) {
+		
+		return repositoryNguyenLieu.save(nguyenlieuForm);
+		}
+}
---
+++ argon-dashboard-master/assets/js/masterpage.js
@@ -0,0 +1,105 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "/examples/dashboard.html"
+  })
+  .when("/icons", {
+    templateUrl : "/examples/dashboard.html"
+  })
+  .when("/tables", {
+    templateUrl : "/examples/tables.html",
+    controller : "ResourcesCtrl"
+  })
+});
+
+app.controller('ResourcesCtrl', function($scope, $http) {
+  $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+  $scope.resources = [
+                //{ Id: "SP000016", Name: "Lemon", type: "Water", price:"14.000", costprice:"8.000", inventory:"30" },
+                //{ Id: "SP000017", Name: "Lemon", type: "Water", price:"14.000", costprice:"8.000", inventory:"30" },
+                //{ Id: "SP000018", Name: "Lemon", type: "Water", price:"14.000", costprice:"8.000", inventory:"30" },
+                //{ Id: "SP000019", Name: "Lemon", type: "Water", price:"14.000", costprice:"8.000", inventory:"30" }
+                ];
+
+  $scope.resourcesType = [
+                //{ Id: 1, Name: "Thịt", Unit: "Kg"},
+                //{ Id: 2, Name: "Cá", Unit: "Kg"},
+                //{ Id: 3, Name: "Nước", Unit: "l"},
+                //{ Id: 4, Name: "Rau", Unit: "Kg"}
+                ];
+
+  //Get resources
+  $http({
+    method : "GET",
+    url : "http://localhost:8080/api/GetAllNguyenLieu"
+  }).then(function mySuccess(response) {
+      $scope.resources = response.data;
+    },function myError(response) {
+      $scope.resources = response.statusText;
+  });
+
+  //Get resources type
+  $http({
+    method : "GET",
+    url : "http://localhost:8080/api/GetAllLoaiNguyenLieu"
+  }).then(function mySuccess(response) {
+      $scope.resourcesType = response.data;
+    });
+
+  //Add resources
+  $scope.updateResources= function(){
+        var data= $.param({
+                    NGUYENLIEU_NAME: $scope.Name,
+                    NGUYENLIEU_PRICE: $scope.Price,
+                    NGUYENLIEU_ID: $scope.resourcesDetails.nguyenlieu_ID,
+                    lOAINGUYENLIEU_LOAINGUYENLIEU_ID: $scope.loaiID,
+
+                });
+
+        $http.post("http://localhost:8080/api/InsertNguyenLieu/",data)
+          .then(function mySuccess(data) {
+          });
+
+      }
+
+      $scope.addResources= function(){
+            var data= $.param({
+                        NGUYENLIEU_NAME: $scope.inputName,
+                        NGUYENLIEU_PRICE: $scope.inputPrice,
+                        lOAINGUYENLIEU_LOAINGUYENLIEU_ID: $scope.TypeId,
+
+                    });
+
+            $http.post("http://localhost:8080/api/InsertNguyenLieu/",data)
+              .then(function mySuccess(data) {
+              });
+
+          }
+
+  $scope.getResourcesIndex=function(resources){
+    $http({
+      method : "GET",
+      url : "http://localhost:8080/api/NguyenLieu/" + resources.nguyenlieu_ID
+    }).then(function mySuccess(response) {
+        $scope.resourcesDetails = response.data;
+      });
+
+  };
+
+  //Delete resources
+  $scope.deleteResources= function(){
+    var data= $.param({
+                empNo: $scope.empNo1
+            });
+    $http.post("url" + data,data)
+      .then(function mySuccess(response){
+        alert("Xoa thanh cong");
+      });
+  }
+});
+
+app.controller('addResources', function($scope, $http) {
+
+
+});
